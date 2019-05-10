b7b37b37a0676ade4394822a7f2ba64494c75169
pietro.partescano <pietro.partescano@schibsted.com>
2019-05-10 19:35:51 +0200

    wip

--- cmd/pigHosts.go
+++ cmd/pigHosts.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+
 	"github.com/docopt/docopt-go"
 )
 
 func main() {
-	usage := `Usage: arguments_example [-vqrh] [FILE] ...
-       arguments_example (--left | --right) CORRECTION FILE
+	usage := `
+  Usage: arguments_example [-vqrh] [FILE] ...
+    arguments_example (--left | --right) CORRECTION FILE
 Process FILE and optionally apply correction to either left-hand side or
-right-hand side.
+  right-hand side.
 Arguments:
-  FILE        optional input file
-  CORRECTION  correction angle, needs FILE, --left or --right to be present
+  FILE        File to process
 Options:
   -h --help
-  -v       verbose mode
-  -q       quiet mode
-  -r       make report
-  --left   use left-hand side
-  --right  use right-hand side`
+  `
 
 	arguments, _ := docopt.Parse(usage, nil, true, "", false)
-	fmt.Println(arguments)
+	fmt.Printf("%v", reflect.TypeOf(arguments["FILE"]))
+}
+
+func cleanString(s string) (string, error) {
+	return "", nil
 }
