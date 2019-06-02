088082411b9193f9937166f411e69df3ef0506a7
pietro partescano <p.pietro@gmail.com>
2019-06-02 17:30:50 +0200

    added log functions

--- cmd/pigHosts/main.go
+++ cmd/pigHosts/main.go
@@ -2,25 +2,71 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"os"
+	"runtime/debug"
 
 	"github.com/docopt/docopt-go"
+	"github.com/sirupsen/logrus"
 )
 
+var VERSION string = "1.0"
+
+func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{DisableTimestamp: true, ForceColors: true, DisableLevelTruncation: true})
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.InfoLevel)
+}
+
 func main() {
-	usage := `
-  Usage: arguments_example [-vqrh] [FILE] ...
-    arguments_example (--left | --right) CORRECTION FILE
-Process FILE and optionally apply correction to either left-hand side or
-  right-hand side.
-Arguments:
-  FILE        File to process
+	usage := `pigHost
+
+Usage: pigHost command [--options] [<arguments>]
+ pigHost (load <file>)
+ pigHost (unload)
+ pigHost (version)
+ pigHost (help | --help | -h)
+
 Options:
-  -h --help
-  `
+ -h, --help    Help online
+ -o, --other   Other params
 
-	arguments, _ := docopt.ParseDoc(usage)
+Command:
+ version    view version
+ unload     disable custom hosts
+ load       load custom hosts from exsternal urls file 
+ help				view online help
 
-	fmt.Printf("%v", reflect.TypeOf(arguments["FILE"]))
+Arguments:
+ file				File to process`
 
+	arguments, err := docopt.ParseDoc(usage)
+	ChkErr(err)
+	r, err := arguments.Bool("help")
+	ChkErr(err)
+	if r {
+		docopt.PrintHelpAndExit(err, usage)
+		os.Exit(0)
+	}
+
+	r, err = arguments.Bool("version")
+	ChkErr(err)
+	if r {
+		logrus.Infof("VERSION: %s", VERSION)
+		os.Exit(0)
+	}
+
+	logrus.Infof("%v, %v\n", r, err)
+
+	err = fmt.Errorf("My Err: %v", "super error!")
+
+	ChkErr(err)
+
+}
+
+// ChkErr check returned error
+func ChkErr(err error) {
+	if err != nil {
+		logrus.WithFields(logrus.Fields{"Stack": string(debug.Stack()), "Verion": VERSION}).Error(err)
+		os.Exit(1)
+	}
 }
