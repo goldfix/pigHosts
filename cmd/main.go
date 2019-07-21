cb74cfd7be87998ad36e00f242d35834bee6d466
pietro partescano <p.pietro@gmail.com>
2019-07-21 17:22:58 +0200

    updated README file

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const VERSION string = "0.2"
+const VERSION string = "1.0"
 
 func init() {
 	logrus.SetFormatter(&logrus.TextFormatter{DisableTimestamp: true, ForceColors: true, DisableLevelTruncation: true})
