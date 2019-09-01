e31422a74156fe439e0d7929ac915581f6dff7a3
pietro.partescano <pietro.partescano@schibsted.com>
2019-09-01 12:06:40 +0200

    add task configuration

--- cmd/pighosts/main.go
+++ cmd/pighosts/main.go
@@ -95,8 +95,8 @@ Command:
 // ChkErr check returned error
 func ChkErr(err error) {
 	if err != nil {
-		logrus.Error(err)
-		logrus.Errorf("Verion: %s", version)
+		logrus.Error(err, "\n\n")
+		logrus.Errorf("Version : %v, commit %v, built at %v", version, commit, date)
 		logrus.Errorf("Stack : %s", string(debug.Stack()))
 		os.Exit(1)
 	}
